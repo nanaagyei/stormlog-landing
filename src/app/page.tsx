c2f0d8ef2eddfdd7a1fafa5a1a77206d22dc43cf
nanaagyei <tuffourp@oregonstate.edu>
2026-06-17 12:53:32 -0700

    address PR comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <EcosystemStrip />
         <WhatsNewSection />
         <ProblemSection />
-        <SpotlightSection />
         <InferenceProfilingSection />
+        <SpotlightSection />
         <WorkflowShowcase />
         <TuiShowcase />
         <BeforeAfterSection />
